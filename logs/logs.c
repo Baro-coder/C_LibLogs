be4ee1cd97d80dd8b43ad6fae0586e623064240c
Baro-coder <b.szykula00@gmail.com>
2024-10-26 19:53:22 +0200

    Semaphore Win32 fuse, default owner fix, copy log stream if NULL or not [stderr, stdout]

--- logs/logs.c
+++ logs/logs.c
@@ -1,24 +1,24 @@
 /*
-*  Copyright (c) 2023 Baro-coder
-* 
-*  Permission is hereby granted, free of charge, to any person obtaining a copy
-*  of this software and associated documentation files (the "Software"), to
-*  deal in the Software without restriction, including without limitation the
-*  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-*  sell copies of the Software, and to permit persons to whom the Software is
-*  furnished to do so, subject to the following conditions:
-* 
-*  The above copyright notice and this permission notice shall be included in
-*  all copies or substantial portions of the Software.
-* 
-*  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-*  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-*  IN THE SOFTWARE.
-*/
+ *  Copyright (c) 2023 Baro-coder
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to
+ *  deal in the Software without restriction, including without limitation the
+ *  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ *  sell copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ *  IN THE SOFTWARE.
+ */
 
 /* ---------------------------------------------------------------------------------------------------- */
 
@@ -29,176 +29,276 @@
 /* -- STATIC VARS -- */
 
 // -- Stream
-static FILE* out_stream = NULL;
+static FILE *out_stream = NULL;
 
+#ifdef _WIN32
+#include <windows.h>
+#include <Lmcons.h>
+
+// -- Get username
+static char *get_username()
+{
+    static char username[UNLEN + 1];
+    DWORD username_len = UNLEN + 1;
+    GetUserName(username, &username_len);
+    if (strlen(username) == 0 || username == NULL || strlen(username) > OWNER_BUFF_SIZE)
+    {
+        return "unknown";
+    }
+    return username;
+}
+#endif
+
+#ifdef __linux__
 // -- Thread-Safety Semaphore
-static sem_t* mtx = NULL;
+static sem_t *mtx = NULL;
+
+// -- Get username
+static char *get_username()
+{
+    struct passwd *pw = getpwuid(getuid());
+    if (pw)
+    {
+        if (strlen(pw->pw_name) == 0 || strlen(pw->pw_name) > OWNER_BUFF_SIZE || pw->pw_name == NULL)
+        {
+            return "unknown";
+        }
+        else
+        {
+            return pw->pw_name;
+        }
+    }
+    else
+    {
+        return "unknown";
+    }
+}
+#endif
+
+// -- Owner default
+#define OWNER_DEFAULT get_username()
 
 // -- Level min
 static log_level_t level_min = LOG_LEVEL_TRACE;
 
 // -- Log level names
-const char* __LEVEL_NAMES[] = {
+const char *__LEVEL_NAMES[] = {
     "  TRACE  ",
     "  DEBUG  ",
     "  INFO   ",
     " WARNING ",
     "  ERROR  ",
-    "  FATAL  "
-};
+    "  FATAL  "};
 
 /* ---------------------------------------------------------------------------------------------------- */
 /* -- PRIVATE FUNCTIONS -- */
 
 // Print log
-void __log_print(log_level_t level, const char* owner, const char* fmt, va_list args) {
+void __log_print(log_level_t level, const char *owner, const char *fmt, va_list args)
+{
     // -- Getting datetime data
-    time_t *now = (time_t*) malloc(sizeof(time_t));
+    time_t *now = (time_t *)malloc(sizeof(time_t));
     time(now);
     struct tm *tm = localtime(now);
 
-    char* timestamp = (char*) malloc(TIMESTAMP_BUFF_SIZE * sizeof(char));
-    sprintf(timestamp, "%02d-%02d-%02d %02d:%02d:%02d", 
-        tm->tm_mday, 
-        tm->tm_mon + 1,
-        tm->tm_year + 1900, 
-        tm->tm_hour, 
-        tm->tm_min, 
-        tm->tm_sec);
+    char *timestamp = (char *)malloc(TIMESTAMP_BUFF_SIZE * sizeof(char));
+    sprintf(timestamp, "%02d-%02d-%02d %02d:%02d:%02d",
+            tm->tm_mday,
+            tm->tm_mon + 1,
+            tm->tm_year + 1900,
+            tm->tm_hour,
+            tm->tm_min,
+            tm->tm_sec);
 
-
+#ifdef __linux__
     // -- Mutex Lock
-    if (mtx != NULL) {
+    if (mtx != NULL)
+    {
         sem_wait(mtx);
     }
+#endif
 
     // -- Log Output
-    if(out_stream != NULL) {
-        fprintf(out_stream, "[%s] | [%s] | [%s (%d)] | ", 
-            timestamp, 
-            __LEVEL_NAMES[level], 
-            owner, 
-            getpid());
-        vfprintf(out_stream, fmt, args);
-        fputc('\n', out_stream);
-    } else {
-        fprintf(stderr, "[%s] | [%s] | [%s (%d)] | ", 
-            timestamp, 
-            __LEVEL_NAMES[level], 
-            owner, 
-            getpid());
+    if (out_stream == NULL)
+    {
+        // -- Output to stderr
+        fprintf(stderr, "[%s] | [%s] | [%s (%d)] | ",
+                timestamp,
+                __LEVEL_NAMES[level],
+                owner,
+                getpid());
+
         vfprintf(stderr, fmt, args);
+        va_end(args);
         fputc('\n', stderr);
+    }
+    else
+    {
+        if (out_stream != stdout && out_stream != stderr)
+        {
+            // -- Copy args
+            va_list args_copy;
+            va_copy(args_copy, args);
+
+            // -- Output copy to stderr
+            fprintf(stderr, "[%s] | [%s] | [%s (%d)] | ",
+                    timestamp,
+                    __LEVEL_NAMES[level],
+                    owner,
+                    getpid());
+            vfprintf(stderr, fmt, args_copy);
+            va_end(args_copy);
+            fputc('\n', stderr);
+        }
+
+        // -- Output to out_stream
+        fprintf(out_stream, "[%s] | [%s] | [%s (%d)] | ",
+                timestamp,
+                __LEVEL_NAMES[level],
+                owner,
+                getpid());
+
+        vfprintf(out_stream, fmt, args);
+        va_end(args);
+        fputc('\n', out_stream);
     }
 
     // -- Memory free
     free(now);
     free(timestamp);
 
+#ifdef __linux__
     // -- Mutex Unlock
-    if(mtx != NULL) {
+    if (mtx != NULL)
+    {
         sem_post(mtx);
     }
+#endif
 }
 
 // Check minimal level
-static bool_t __check_min_level(log_level_t level) {
+static bool_t __check_min_level(log_level_t level)
+{
     return (level >= level_min);
 }
 
 // Check level and print log
-void __log(log_level_t level, const char* owner, const char* fmt, va_list args) {
-    if(!__check_min_level(level)) {
+void __log(log_level_t level, char *owner, const char *fmt, va_list args)
+{
+    if (!__check_min_level(level))
+    {
         return;
     }
+    if (owner == NULL || owner == "" || owner == " " || owner[0] == '\0')
+    {
+        owner = OWNER_DEFAULT;
+    }
+
     __log_print(level, owner, fmt, args);
 }
 
 /* ---------------------------------------------------------------------------------------------------- */
 /* -- PUBLIC FUNCTIONS -- */
 
+#ifdef __linux__
 // ---- Threads-Safety semaphore init
-int logs_threads_safety_enable(const char* sem_name) {
-    mtx = (sem_t*) malloc(sizeof(sem_t));
+int logs_threads_safety_enable(const char *sem_name)
+{
+    mtx = (sem_t *)malloc(sizeof(sem_t));
     mtx = sem_open(sem_name, O_CREAT | O_EXCL, 0666, 1);
-    if (mtx == SEM_FAILED) {
+    if (mtx == SEM_FAILED)
+    {
         return 1;
     }
     return 0;
 }
 // ---- Threads-Safety semaphore destroy
-int logs_threads_safety_disable(const char* sem_name) {
-    if (sem_close(mtx) < 0) {
+int logs_threads_safety_disable(const char *sem_name)
+{
+    if (sem_close(mtx) < 0)
+    {
         return 2;
     }
-    
+
     mtx = NULL;
 
-    if (sem_unlink(sem_name) < 0) {
+    if (sem_unlink(sem_name) < 0)
+    {
         return 1;
     }
 
     return 0;
 }
+#endif
 
 // ---- Minimal log level set
-void logs_set_minimal_log_level(log_level_t level) {
+void logs_set_minimal_log_level(log_level_t level)
+{
     level_min = level;
 }
 
 // ---- Output stream set `stream`
-void logs_set_output_stream(FILE* stream) {
+void logs_set_output_stream(FILE *stream)
+{
     out_stream = stream;
 }
 // ---- Output stream set DEFAULT
-void logs_set_output_stream_default() {
+void logs_set_output_stream_default()
+{
     logs_set_output_stream(NULL);
 }
 
 // ---- 'SPECIFIED' LOG
-void logs_log(log_level_t level, const char* owner, const char* fmt, ...) {
+void logs_log(log_level_t level, const char *owner, const char *fmt, ...)
+{
     va_list args;
     va_start(args, fmt);
     __log(level, owner, fmt, args);
     va_end(args);
 }
 // ---- TRACE LOG
-void logs_log_trace(const char* owner, const char* fmt, ...) {
+void logs_log_trace(const char *owner, const char *fmt, ...)
+{
     va_list args;
     va_start(args, fmt);
     __log(LOG_LEVEL_TRACE, owner, fmt, args);
     va_end(args);
 }
 // ---- DEBUG LOG
-void logs_log_debug(const char* owner, const char* fmt, ...) {
+void logs_log_debug(const char *owner, const char *fmt, ...)
+{
     va_list args;
     va_start(args, fmt);
     __log(LOG_LEVEL_DEBUG, owner, fmt, args);
     va_end(args);
 }
 // ---- INFO LOG
-void logs_log_info(const char* owner, const char* fmt, ...) {
+void logs_log_info(const char *owner, const char *fmt, ...)
+{
     va_list args;
     va_start(args, fmt);
     __log(LOG_LEVEL_INFO, owner, fmt, args);
     va_end(args);
 }
 // ---- WARNING LOG
-void logs_log_warn(const char* owner, const char* fmt, ...) {
+void logs_log_warn(const char *owner, const char *fmt, ...)
+{
     va_list args;
     va_start(args, fmt);
     __log(LOG_LEVEL_WARNING, owner, fmt, args);
     va_end(args);
 }
 // ---- ERROR LOG
-void logs_log_error(const char* owner, const char* fmt, ...) {
+void logs_log_error(const char *owner, const char *fmt, ...)
+{
     va_list args;
     va_start(args, fmt);
     __log(LOG_LEVEL_ERROR, owner, fmt, args);
     va_end(args);
 }
 // ---- FATAL LOG
-void logs_log_fatal(const char* owner, const char* fmt, ...) {
+void logs_log_fatal(const char *owner, const char *fmt, ...)
+{
     va_list args;
     va_start(args, fmt);
     __log(LOG_LEVEL_FATAL, owner, fmt, args);
--- logs/logs.h
+++ logs/logs.h
@@ -1,21 +1,21 @@
 /*
-*  Copyright (c) 2023 Baro-coder
-* 
-*  This library is free software; you can redistribute it and/or modify it
-*  under the terms of the GNU GENERAL PUBLIC LICENSE v3. See `logs.c` for details.
-*/
+ *  Copyright (c) 2023 Baro-coder
+ *
+ *  This library is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU GENERAL PUBLIC LICENSE v3. See `logs.c` for details.
+ */
 
 /* *** Info *** */
 /*
-*   -- Formats --
-*  Log:          [DATETIME] : [LEVEL] : [OWNER] : MESSAGE
-*  Datetime:     %dd-%MM-%yyyy %hh:%mm:%ss
-*  
-*   -- Examples --
-*   [5-04-2023 17:49:41] : [ DEBUG ] : [ App ] : Control mechanisms init...
-*   [5-04-2023 17:52:32] : [ INFO  ] : [ C-4 ] : Init...
-*   [5-04-2023 17:53:16] : [ INFO  ] : [ C-4 ] : Ready.
-*/
+ *   -- Formats --
+ *  Log:          [DATETIME] : [LEVEL] : [OWNER] : MESSAGE
+ *  Datetime:     %dd-%MM-%yyyy %hh:%mm:%ss
+ *
+ *   -- Examples --
+ *   [5-04-2023 17:49:41] : [ DEBUG ] : [ App ] : Control mechanisms init...
+ *   [5-04-2023 17:52:32] : [ INFO  ] : [ C-4 ] : Init...
+ *   [5-04-2023 17:53:16] : [ INFO  ] : [ C-4 ] : Ready.
+ */
 
 #ifndef LOGS_H
 #define LOGS_H
@@ -23,76 +23,114 @@
 /* *** INCLUDES *** */
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <stdarg.h>
+#ifdef __linux__
 #include <semaphore.h>
+#endif
 #include <fcntl.h>
 #include <time.h>
 
-
 /* *** DEFINITIONS *** */
 
-#define TIMESTAMP_BUFF_SIZE 20
+#define OWNER_BUFF_SIZE 128    // Buffer size for owner name
+#define TIMESTAMP_BUFF_SIZE 72 // Buffer size for timestamp
 
 // Custom boolean type
 typedef int bool_t;
 
 // -- Log levels
 // Log levels enumeration
-enum __log_level_t{
-    LOG_LEVEL_TRACE, 
-    LOG_LEVEL_DEBUG, 
-    LOG_LEVEL_INFO, 
-    LOG_LEVEL_WARNING, 
-    LOG_LEVEL_ERROR, 
-    LOG_LEVEL_FATAL};
-
-typedef enum __log_level_t log_level_t;
-
+typedef enum __log_level_t
+{
+    LOG_LEVEL_TRACE = 0,   // Trace
+    LOG_LEVEL_DEBUG = 1,   // Debug
+    LOG_LEVEL_INFO = 2,    // Info
+    LOG_LEVEL_WARNING = 3, // Warning
+    LOG_LEVEL_ERROR = 4,   // Error
+    LOG_LEVEL_FATAL = 5    // Fatal
+} log_level_t;
 
 /* *** INTERFACE *** */
 
 /* --- OPTIONS */
 
-// Creating semaphore with name as `sem_name` to sync threads printing.
-//  `Attention`: Remember to call `logs_threads_safety_disable()` to remove created semaphore!
-//  Returns: `0` on success; `1` - sem_open error
-int logs_threads_safety_enable(const char* sem_name);
+#ifdef __linux__
+/// @brief Creating semaphore with name as `sem_name` to sync threads printing.
+/// @attention Remember to call `logs_threads_safety_disable()` to remove created semaphore!
+/// @param sem_name semaphore name
+/// @return `0` on success; `1` - sem_open error
+int logs_threads_safety_enable(const char *sem_name);
 
-// Removing semaphore with name as `sem_name` to sync threads printing.
-//  Returns: `0` on success; `1` - sem_unlink error; `2` - sem_close error
-int logs_threads_safety_disable(const char* sem_name);
+/// @brief Removing semaphore with name as `sem_name` to sync threads printing.
+/// @param sem_name semaphore name
+/// @return `0` on success; `1` - sem_unlink error; `2` - sem_close error
+int logs_threads_safety_disable(const char *sem_name);
+#endif
 
-// Set minimal log level to `level`
+/// @brief Set minimal log level to `level`
+/// @param level minimal log level
 void logs_set_minimal_log_level(log_level_t level);
 
-// Set log output stream to `stream`
-void logs_set_output_stream(FILE* stream);
+/// @brief Set log output stream to `stream`
+/// @param stream output stream
+/// @attention If `stream` is `NULL`, output stream will be set to `stderr`
+void logs_set_output_stream(FILE *stream);
 
-// Set log output stream to `stderr`
+/// @brief Set log output stream to `stderr`
 void logs_set_output_stream_default();
 
 /* --- LOG WRITERS */
 
 // Write `level` log with `owner` and message as formatted output
-void logs_log(log_level_t level, const char* owner, const char* fmt, ...);
 
-// Write TRACE log with `owner` and message as formatted output
-void logs_log_trace(const char* owner, const char* fmt, ...);
+/// @brief Write log with `level`, `owner` and message as formatted output
+/// @param level log level
+/// @param owner log owner
+/// @param fmt message format
+/// @param ... message arguments
+/// @attention If `owner` is `NULL`, it will be set to name of user who called the function
+void logs_log(log_level_t level, char *owner, const char *fmt, ...);
 
-// Write DEBUG log with `owner` and message as formatted output
-void logs_log_debug(const char* owner, const char* fmt, ...);
+/// @brief Write TRACE log with `owner` and message as formatted output
+/// @param owner log owner
+/// @param fmt message format
+/// @param  ... message arguments
+/// @attention If `owner` is `NULL`, it will be set to name of user who called the function
+void logs_log_trace(const char *owner, const char *fmt, ...);
 
-// Write INFO log with `owner` and message as formatted output
-void logs_log_info(const char* owner, const char* fmt, ...);
+/// @brief Write TRACE log with `owner` and message as formatted output
+/// @param owner log owner
+/// @param fmt message format
+/// @param  ... message arguments
+/// @attention If `owner` is `NULL`, it will be set to name of user who called the function
+void logs_log_debug(const char *owner, const char *fmt, ...);
 
-// Write WARNING log with `owner` and message as formatted output
-void logs_log_warn(const char* owner, const char* fmt, ...);
+/// @brief Write INFO log with `owner` and message as formatted output
+/// @param owner log owner
+/// @param fmt message format
+/// @param  ... message arguments
+/// @attention If `owner` is `NULL`, it will be set to name of user who called the function
+void logs_log_info(const char *owner, const char *fmt, ...);
 
-// Write ERROR log with `owner` and message as formatted output
-void logs_log_error(const char* owner, const char* fmt, ...);
+/// @brief Write WARNING log with `owner` and message as formatted output
+/// @param owner log owner
+/// @param fmt message format
+/// @param  ... message arguments
+/// @attention If `owner` is `NULL`, it will be set to name of user who called the function
+void logs_log_warn(const char *owner, const char *fmt, ...);
 
-// Write FATAL log with `owner` and message as formatted output
-void logs_log_fatal(const char* owner, const char* fmt, ...);
+/// @brief Write ERROR log with `owner` and message as formatted output
+/// @param owner log owner
+/// @param fmt message format
+/// @param  ... message arguments
+/// @attention If `owner` is `NULL`, it will be set to name of user who called the function
+void logs_log_error(const char *owner, const char *fmt, ...);
+
+/// @brief Write FATAL log with `owner` and message as formatted output
+/// @param owner log owner
+/// @param fmt message format
+/// @param  ... message arguments
+/// @attention If `owner` is `NULL`, it will be set to name of user who called the function
+void logs_log_fatal(const char *owner, const char *fmt, ...);
 
 #endif
